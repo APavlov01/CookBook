2a0754ef389c7b0b9091997d4ab82f324ee235e2
APavlov01 <a.pavvlov@gmail.com>
2019-03-25 11:18:36 +0200

    New project .NETCore

---
+++ CookBook/Data/Models/Rating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook
+{
+    class Rating
+    {
+        public double Score { get; set; }
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+        public Rating()
+        {
+
+        }
+        public Rating(double score)
+        {
+            this.Score = score;
+        }
+
+    }
+}
---
+++ CookBook/Data/RecipeContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookBook
+{
+    class RecipeContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

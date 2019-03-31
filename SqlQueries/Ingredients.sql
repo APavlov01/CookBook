57d05014423a34da6243a1661aa3a01da43f142d
ivanbadikov01 <ibadikovtues2020@gmail.com>
2019-03-30 17:58:04 -0700

    Update Ingredients.sql

--- SqlQueries/Ingredients.sql
+++ SqlQueries/Ingredients.sql
@@ -1,5 +1,29 @@
-CREATE database CookBook;
-
 use CookBook;
 
 select * from Ingredients;
+
+/*insert into Ingredients (Name, Type, Calories) values ('tomato', 'g', '18');*/
+insert into Ingredients (Name, Type, Calories) values ('butter', 'g', '717');
+insert into Ingredients (Name, Type, Calories) values ('cheddar', 'g', '404');
+insert into Ingredients (Name, Type, Calories) values ('mozarella', 'g', '300');
+insert into Ingredients (Name, Type, Calories) values ('parmesan', 'g', '420');
+insert into Ingredients (Name, Type, Calories) values ('ricotta', 'g', '174');
+insert into Ingredients (Name, Type, Calories) values ('swiss', 'g', '393');
+insert into Ingredients (Name, Type, Calories) values ('american', 'g', '126');
+insert into Ingredients (Name, Type, Calories) values ('cream', 'g', '123');
+insert into Ingredients (Name, Type, Calories) values ('milk', 'g', '61');
+insert into Ingredients (Name, Type, Calories) values ('egg-whites', '', '52');
+insert into Ingredients (Name, Type, Calories) values ('egg-yolk', '', '322');
+insert into Ingredients (Name, Type, Calories) values ('potato', 'g', '77');
+insert into Ingredients (Name, Type, Calories) values ('mayonnaise', 'g', '361');
+insert into Ingredients (Name, Type, Calories) values ('sunflower-oil', 'g', '884');
+insert into Ingredients (Name, Type, Calories) values ('canola-oil', 'g', '884');
+insert into Ingredients (Name, Type, Calories) values ('olive-oil', 'g', '884');
+insert into Ingredients (Name, Type, Calories) values ('palm-oil', 'g', '884');
+insert into Ingredients (Name, Type, Calories) values ('grapeseed-oil', 'g', '884');
+insert into Ingredients (Name, Type, Calories) values ('beef-shortrib', 'g', '388');
+insert into Ingredients (Name, Type, Calories) values ('beef-brisket', 'g', '221');
+insert into Ingredients (Name, Type, Calories) values ('beef-ribeye', 'g', '149');
+insert into Ingredients (Name, Type, Calories) values ('beef-chuck', 'g', '216');
+insert into Ingredients (Name, Type, Calories) values ('beef-rib', 'g', '372');
+insert into Ingredients (Name, Type, Calories) values ('beef-loin', 'g', '211');

5d12b2a356347f54954c2360a1eab57bbce28a6d
ivanbadikov01 <ibadikovtues2020@gmail.com>
2019-03-31 19:37:35 +0300

    New methods to separate reading data

--- CookBook/Business Logic/Controller.cs
+++ CookBook/Business Logic/Controller.cs
@@ -11,6 +11,7 @@ namespace CookBook
         private Display display = new Display();
         private RecipeContext recipeContext = new RecipeContext();
         private string command;
+        private string result;
 
         public Controller()
         {
@@ -32,26 +33,28 @@ namespace CookBook
         private void Add()
         {
             string recipeName = null;
-
-            List<Ingredient> ingredients = new List<Ingredient>();
-
-            string ingredientArgs = null;
-
+            string ingredients = null;            
             string description = null;
-
-            string result = "";
-
-            int validator = 0;
+            
 
             display.AddCmdDisplay();
+            recipeName = recipeRead();
+            ingredients = IngredientsRead();            
+            description = DescriptionRead();
+        }
+
+        private string recipeRead()
+        {
+            string recipeName = null;
+            int validator = 0;
 
             do
             {
                 recipeName = display.GetRecipeName();
 
-                validator=ValidateRecipeName(recipeName);
+                validator = ValidateRecipeName(recipeName);
 
-                if(validator == 0)
+                if (validator == 0)
                 {
                     result = "Name already used!";
                 }
@@ -70,61 +73,99 @@ namespace CookBook
                 display.PrintResult(result);
                 //TO DO display result -karatov =====> DONE
 
-            } while (ValidateRecipeName(recipeName)!=1);
+            } while (ValidateRecipeName(recipeName) != 1);
 
             display.PrintResult(result);
 
+            return recipeName;
+        }
 
-            result = "";
+        private string IngredientsRead()
+        {
+            string ingredientArgs = null;
+            int validator = 0;
+            bool continueReading = true;
+            string ingredients = null;
+
             do
             {
-                display.PrintResult(result);
-
                 ingredientArgs = display.GetIngredients();
 
-                validator = ValidateIngredients(ingredientArgs);
-
-                if (validator == 1)
+                if (!ingredientArgs.Equals("end"))
                 {
-                    result = "Successfuly added ingredient!\n";
-                    //var ingredient = IngredientParsing(ingredientArgs);
-                    //ingredients.Add(ingredient);
+                    validator = ValidateIngredients(ingredientArgs);
+
+                    if (validator == 1)
+                    {
+                        result = "Successfuly added ingredient!\n";
+                        //var ingredient = IngredientParsing(ingredientArgs);
+                        //ingredients.Add(ingredient);
+                    }
+
+                    else if (validator == -1)
+                    {
+                        result = "Invalid quantity input!\n";
+                    }
+
+                    else if (validator == 0)
+                    {
+                        result = "Ingredient doesn't exist!\n";
+                    }
+                    display.PrintResult(result);
                 }
 
-                else if(validator==-1)
+                else
                 {
-                    result = "Invalid quantity input!\n";
+                    continueReading = false;
                 }
 
-                else if(validator==0)
-                {
-                    result = "Ingredient doesn't exist!\n";
-                }
+            } while (continueReading);
 
-            } while (!ingredientArgs.ToLower().Equals("end"));
             result = "Finished adding ingredients!\n";
 
             display.PrintResult(result);
 
+            return ingredients;  //TO DO implement IngredientParsing() method 
+        }
+
+        //TO DO Add everything to database
+
+        private string DescriptionRead()
+        {
+            int validator = 0;
+            string description = null;
+
             StringBuilder sb = new StringBuilder();
             do
             {
-                string descriptionArg = display.GetDescription();
+                string paragraph = display.GetDescription();
 
-                validator = ValidateDescription(descriptionArg);
+                validator = ValidateDescription(paragraph);
+                
+                if (validator == 1)
+                {
+                    result = "Successfully added description!";
+                }
+                else if (validator == -1)
+                {
+                    result = "Please enter a valid paragraph.";
+                    continue;
+                }
 
-                sb.Append(descriptionArg + "\n");
+                sb.Append(paragraph + "\n");
 
-                result = sb.ToString();
-                result = result.Remove(result.Length - 2);
             } while (validator != 1);
+
+            description = sb.ToString().Remove(description.Length - 2);
+
             display.PrintResult(result);
+
+            return description;
         }
-        //TO DO Add everything to database
 
         private int ValidateDescription(string description)
         {
-            if (string.IsNullOrEmpty(description))
+            if (string.IsNullOrEmpty(description) || description.Equals("#"))
             {
                 return -1;
             }
@@ -207,7 +248,6 @@ namespace CookBook
             display.SearcheCmdDisplay();
             display.GetRecipeName();
         }
-
-        //TO DO additional methods for the do whiles
+        
     }
 }

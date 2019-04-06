76a02deb7c68163b0e8be62230fce4434931c40c
Alex-K-2020v <32852359+Alex-K-2020v@users.noreply.github.com>
2019-04-07 00:34:48 +0300

    All 4 recipes added to db
    Remember to remove the last ingredient in each recipe (it is a tomato with 22 g of weight) to update the calories.

--- SqlQueries/Recipes.sql
+++ SqlQueries/Recipes.sql
@@ -1,21 +1,18 @@
 use CookBook
 select * from Recipes;
+select * from Ingredients;
 
-insert into Recipes(Name, Ingredients, Description, Calories) values ('Poached eggs', 'vinegar 15;egg 100;tomato 22', '15ml is equal to 1 tablespoon, Two medium sized eggs weigh 100 g.
+insert into Recipes(Name, Ingredients, Description, Calories) values ('Poached eggs', 'vinegar 15;egg 100;tomato 22', 
+'15ml is equal to 1 tablespoon, Two medium sized eggs weigh 100 g.
+
 -Fill a small pan just over one third full with cold water and bring it to the boil.
-
 -Add the vinegar and turn down to simmer.
-
 -Crack the eggs one at a time into a small bowl and gently tip into the simmering water. Lightly poach for 2-3 minutes.
-
 -Remove with a slotted spoon and drain on kitchen towels.
-
 -Season with salt and freshly ground black pepper and serve immediately.
 
 -Recipe Tips
-
 Use the freshest eggs for best results – the whites will hold together better.
-
 Another trick is to gently place the egg in a fine sieve before cooking. This gets rid of the watery parts of the white, which go stringy when cooked and cause the water to go cloudy.','22');
 
 
@@ -25,3 +22,26 @@ insert into Recipes(Name, Ingredients, Description, Calories) values ('Old-fashi
 -Remove the beef from the pot and add the vinegar and wine. Cook over medium-high heat, scraping the pan with a wooden spoon to loosen any browned bits. Add the beef, beef broth and bay leaves. Bring to a boil, then reduce to a slow simmer.
 -Cover and cook, skimming broth from time to time, until the beef is tender, about 1 1/2 hours. Add the onions and carrots and simmer, covered, for 10 minutes. Add the potatoes and simmer until vegetables are tender, about 30 minutes more. Add broth or water if the stew is dry. Season with salt and pepper to taste. Ladle among 4 bowls and serve.
 ','22');
+
+insert into Recipes(Name, Ingredients, Description, Calories) values ('Tarator', 'yoghurt 500;cucumber 200;garlic-fresh 20;water 1000;salt 5;sunflower-oil 10;tomato 22',
+ '-Beat the yogurt until it is smooth.
+-Cut the garlic in little pieces.
+-With a mixer mix the yoghurt, cucumber, the garlic, the sunflower oil, and the water.
+-Mix until reaching a smooth consistency. Add salt.
+-Serve chilled or top with ice cubes.','22');
+
+
+insert into Recipes(Name, Ingredients, Description, Calories) values ('Apple cake', 'flour 200;sugar 150;yoghurt 200;egg 100;baking-powder 16;sunflower-oil 30;apple 200;cinnamon 5;butter 40;tomato 22',
+ '-Preheat the oven to 180° C (350 degrees F). Butter and flour a 22 cm ( 9-inch ) pan and set aside.
+-Peel, core and cut the apples into thin slices. Place in a large mixing bowl and toss with lemon juice to avoid discoloration. Set aside.
+-Using an electric mixer, beat the eggs and sugar until pale and fluffy.
+-Add the oil, half of the cream, flour, baking powder, and mix well.
+-Add the remaining cream and mix again.
+-Pour the mixture into the prepared pan.
+-Arrange the apples slices over the top of the batter in a circular pattern.
+-Sprinkle the apple slices with sugar, cinnamon and dot with bits of butter here and there.
+-Bake for 45 minutes or until golden brown and a skewer inserted into the centre of the cake comes out clean.
+-Cool before removing from the pan and dust with powdered sugar before serving.
+-Enjoy your apple cake!','22');
+
+
